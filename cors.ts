c312901769d5899c7a33faee652f4fd8957f019d
DESKTOP-J2463JH\fcurt <fcurti1998@outlook.com>
2023-08-11 23:09:33 -0300

    aplicação do cors em typescript afim de configurar um servidor genérico

---
+++ cors.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
+app.listen(8080, () => {
+  console.log('Servidor rodando na porta 8080');
+});
